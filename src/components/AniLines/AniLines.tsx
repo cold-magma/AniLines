3f4a8857487c8a2906c06dab6c9950e0169ed334
Amal Nair <amalrocks29@gmail.com>
2023-03-06 10:29:49 +0530

    Initial Commit
    The first commit for this project

---
+++ src/components/AniLines/AniLines.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { Row } from "../Row/Row";
+
+const AniLines = () => {
+    const [mouseInside, setMouseInside] = useState(false);
+    let angle: number = 0;
+    let x: number;
+
+    const [color, setColor] = useState({ r: 255, g: 255, b: 255 });
+
+    useEffect(() => {
+        animateLines();
+    }, [mouseInside]);
+
+    const mouseFollow = (e:any) => {
+        if (x != null || x != undefined) {
+            clearInterval(x);
+        }
+        setMouseInside(true);
+        var lines = document.getElementsByClassName("line");
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i] as HTMLSpanElement;
+
+            var x1: number = e.pageX;
+            var y1: number = e.pageY;
+
+            var x2: number = line.offsetLeft;
+            var y2: number = line.offsetTop;
+
+            let deltaX: number = x2 - x1;
+            let deltaY: number = y2 - y1;
+            let radians: number = Math.atan2(deltaY, deltaX);
+            let degrees: number = (radians * 180) / Math.PI - 90; // rotate
+            if (degrees > 360) {
+                degrees -= 360;
+            }
+            if (degrees < 0) {
+                degrees += 360;
+            }
+            if (degrees == 360) {
+                degrees = 0;
+            }
+
+            line.style.transform = "rotate(" + degrees + "deg) ";
+        }
+    };
+
+    const mouseLeft = () => {
+        setMouseInside(false);
+    };
+
+    const animateLines = () => {
+        x = setInterval(() => theActualAnimation(), 100);
+    };
+
+    const theActualAnimation = () => {
+        if (mouseInside) {
+            return;
+        }
+        var lines = document.getElementsByClassName("line");
+        var degrees: number = angle;
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i] as HTMLSpanElement;
+
+            if (angle > 360) {
+                angle -= 360;
+            }
+            if (angle < 0) {
+                angle += 360;
+            }
+            if (angle == 360) {
+                angle = 0;
+            }
+            degrees += 5;
+            if (degrees > 360) {
+                degrees -= 360;
+            }
+            if (degrees < 0) {
+                degrees += 360;
+            }
+            if (degrees == 360) {
+                degrees = 0;
+            }
+            line.style.transform = "rotate(" + degrees + "deg) ";
+        }
+        angle += 5;
+    };
+
+    return (
+        <div
+            onMouseMove={mouseFollow}
+            onMouseLeave={mouseLeft}
+            className="App"
+            id="maindiv"
+        >
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+        </div>
+    );
+}
+
+export default AniLines;
---
+++ src/components/Row/Row.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Row = () => {
+    return (
+        <div className="row">
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+        </div>
+    );
+}
